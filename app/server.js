5f7ea49e434423dfffd89fe892aeef78d520f1a8
Damien Joris <damien.joris@promyze.com>
2021-04-07 20:04:38 +0200

    Add socket io, io reference error on client side, investigating

--- app/server.js
+++ app/server.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
 
-var express = require("express");
-const app = express();
+const express = require("express");
 const path = require("path");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+
 const { startPlayer, stopPlayer } = require("./publish.service");
 const { initRedisSubscribe } = require("./subscribe.service");
 
@@ -20,6 +24,10 @@ app.get("/start-player", (req, res) => {
 app.get("/stop-player", (req, res) => {
   stopPlayer();
   res.end();
+});
+
+io.on("connection", socket => {
+  console.log("a user connected");
 });
 
 app.listen(3000, function () {
